47128cecf14591d80168f65d58b27fc88d6216ca
Hassen dhaouadi <Dhaouadi002@gmail.com>
2021-02-23 22:11:08 +0300

    add first GET route

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+const userDetails = {
+    fullName: "Ali ben Salah",
+    age: 30,
+    nationality: "Tunisien"
+}
+app.get('/', returnUser);
+
+function returnUser(req, res) {
+    res.send(userDetails);
+};
 app.listen(8000, function() {
     console.log("server is running")
 })
