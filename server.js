1a1b8fd5c1a6166b9143b3e9b444f75753236e18
Hassen dhaouadi <Dhaouadi002@gmail.com>
2021-02-24 00:30:53 +0300

    add login POST route

--- server.js
+++ server.js
@@ -1,17 +1,54 @@
 const express = require('express');
 const app = express();
-const userDetails = {
-    fullName: "Ali ben Salah",
-    age: 30,
-    nationality: "Tunisien"
-}
-app.get('/user/:id', returnUser);
+const bodyParser = require('body-parser');
 
-function returnUser(req, res) {
-    var id = req.params.id;
-    console.log(id);
-    res.send(userDetails);
-};
-app.listen(8000, function() {
-    console.log("server is running")
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const mockUserData = [
+    { name: 'Mark' },
+    { name: 'Jill' }
+]
+
+app.get('/users', function(req, res) {
+        res.json({
+            success: true,
+            message: 'successfully got users. Nice!',
+            users: mockUserData
+        })
+    })
+    // colons are used as variables that be viewed in the params
+app.get('/users/:id', function(req, res) {
+    console.log(req.params.id)
+    res.json({
+        success: true,
+        message: 'got one user',
+        user: req.params.id
+    })
 })
+
+app.post('/login', function(req, res) {
+    // Typically passwords are encrypted using something like bcrypt before sending to database
+    const username = req.body.username;
+    const password = req.body.password;
+    console.log(JSON.stringify(req.body));
+    // This should come from the database
+    const mockUsername = "billyTheKid";
+    const mockPassword = "superSecret";
+
+    if (username === mockUsername && password === mockPassword) {
+        // In practice, use JSON web token sign method here to make an encrypted token
+        res.json({
+            success: true,
+            message: 'password and username match!',
+            token: 'encrypted token goes here'
+        })
+    } else {
+        res.json({
+            success: false,
+            message: 'password and username do not match'
+        })
+    }
+
+})
+
+app.listen(8000, function() { console.log('server is listening') })
